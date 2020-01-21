332c21268d105483f53077d85156547d19ed83f6
martagrus <mrs_marta@o2.pl>
2020-01-21 21:01:26 +0100

    adding firebase menu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let $breakfast, $brunch, $lunch, $dinner, $desserts, $drinks, $delete;
+let $breakfast, $brunch, $lunch, $dinner, $desserts, $drinks, $delete, $table;
 
 main = () => {
     prepareElements();
@@ -14,15 +14,16 @@ prepareElements = () => {
     $drinks = document.getElementById('drinks');
     $title = document.getElementById('modal-title');
     $delete = document.getElementById('delete');
+    $table = document.getElementById('table');
 }
 
 prepareEvents = () => {
-    $breakfast.addEventListener('click', openModal);
-    $brunch.addEventListener('click', openModal);
-    $lunch.addEventListener('click', openModal);
-    $dinner.addEventListener('click', openModal);
-    $desserts.addEventListener('click', openModal);
-    $drinks.addEventListener('click', openModal);
+    $breakfast.addEventListener('click', updateBreakfastMenu);
+    $brunch.addEventListener('click', updateBrunchMenu);
+    $lunch.addEventListener('click', updateLunchMenu);
+    $dinner.addEventListener('click', updateDinnerMenu);
+    $desserts.addEventListener('click', updateDessertsMenu);
+    $drinks.addEventListener('click', updateDrinksMenu);
     $delete.addEventListener('click', closeModal);
 }
 
@@ -30,7 +31,79 @@ openModal = () => {
     document.getElementById('modal').classList.add('is-active');
 }
 
+updateBreakfastMenu = async () => {
+    let menu = await axios.get("https://fake-restaurant-menu.firebaseio.com/.json")
+        menu.data.Breakfast.forEach(item => {
+            addNewItem(item.name, item.price)
+        });
+        openModal();
+}
+
+updateBrunchMenu = async () => {
+    let menu = await axios.get("https://fake-restaurant-menu.firebaseio.com/.json")
+        menu.data.Brunch.forEach(item => {
+            addNewItem(item.name, item.price)
+        });
+        openModal();
+}
+
+updateLunchMenu = async () => {
+    let menu = await axios.get("https://fake-restaurant-menu.firebaseio.com/.json")
+        menu.data.Lunch.forEach(item => {
+            addNewItem(item.name, item.price)
+        });
+        openModal();
+}
+
+updateDinnerMenu = async () => {
+    let menu = await axios.get("https://fake-restaurant-menu.firebaseio.com/.json")
+        menu.data.Dinner.forEach(item => {
+            addNewItem(item.name, item.price)
+        });
+        openModal();
+}
+
+updateDessertsMenu = async () => {
+    let menu = await axios.get("https://fake-restaurant-menu.firebaseio.com/.json")
+        menu.data.Desserts.forEach(item => {
+            addNewItem(item.name, item.price)
+        });
+        openModal();
+}
+
+updateDrinksMenu = async () => {
+    let menu = await axios.get("https://fake-restaurant-menu.firebaseio.com/.json")
+        menu.data.Drinks.forEach(item => {
+            addNewItem(item.name, item.price)
+        });
+        openModal();
+}
+
+addNewItem = (name, price) => {
+    const newItem = createMenuItem(name, price);
+    $table.appendChild(newItem);
+}
+
+createMenuItem = (name, price) => {
+    let newName = document.createElement('td');
+    newName.innerText = name;
+
+    let newPrice = document.createElement('td');
+    newPrice.innerText = price;
+
+    let newItem = document.createElement('tbody');
+    newItem.appendChild(newName)
+    newItem.appendChild(newPrice)
+    
+    return newItem;
+}
+
 closeModal = () => {
+    let list = document.getElementsByTagName('tbody');
+    for (let i = list.length - 1; i >= 0; --i) {
+        list[i].remove();
+      }
+
     document.getElementById('modal').classList.remove('is-active');
 }
 
